fd2672601b45f8ab1d17e7e43ef4659c1e44bb6a
abbodev <abbodev@gmail.com>
2023-05-03 21:53:43 +0200

    feat: installed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    rootId: 'app'
-  }
-})
+    rootId: "app",
+  },
+  modules: ["@pinia/nuxt"],
+  pinia: {
+    autoImports: ["defineStore", "storeToRefs"],
+  },
+});
